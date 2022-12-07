3516c534962425b95713ec6d515138a9d4816cf7
Marcel-harvey <marcelharvey1@gmail.com>
2022-12-07 13:37:09 +0200

    Removed unneeded stuff from skeleton

--- website/__init__.py
+++ website/__init__.py
@@ -1,20 +1,10 @@
 from flask import Flask, render_template, session
-from flask_sqlalchemy import SQLAlchemy
 from os import path
-from flask_login import LoginManager
-from flask_restful import Api, Resource
 
-
-database = SQLAlchemy()
-DB_NAME = "picker.db"
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config['SECRET_KEY'] = "123456789"
-    
-    # Settting up the database for forms
-    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
-    database.init_app(app)    
     
     # Relative imports for all the views
     from .views import views
@@ -25,25 +15,5 @@ def create_app():
     # Import the database that was created for the users from models.py
     from .models import User, Parts, PickingSlip
     
-    '''
-    with app.app_context():
-        database.create_all()
-        '''
-    
-    # Used so that user can access some pages that others cant
-    login_manager = LoginManager()
-    login_manager.login_view = "auth.login"
-    login_manager.init_app(app)
-    
-    @login_manager.user_loader
-    def load_user(id):
-        return User.query.get(int(id))
-    
     return app
-
-
-def create_database(app):
-    # Creates the database if it does not eist
-    if not path.exists("website/" + DB_NAME):
-        database.create_all(app=app)
         
--- website/models.py
+++ website/models.py
@@ -1,33 +1,12 @@
 from . import database
-from flask_login import UserMixin
-from sqlalchemy.sql import func
 
 
-class User(database.Model, UserMixin):
+class User(database.Model):
     # Creates the columns for the user
     id = database.Column(database.Integer, primary_key=True)
     username = database.Column(database.String(50), unique=True)
     password = database.Column(database.String(150))
     # Will be generated randomly with every user
     token = database.Column(database.String(30), unique=True)
-    date_created = database.Column(database.DateTime(timezone=True), default=func.now())
-    
-
-class Parts(database.Model):
-    # Creates the columns for the parts
-    id = database.Column(database.Integer, primary_key=True)
-    part_number = database.Column(database.Text, nullable=False)
-    part_name = database.Column(database.Text, nullable=False)
-    quantity = database.Column(database.Text, nullable=False)
-    notes = database.Column(database.Text)
-
-
-class PickingSlip(database.Model):
-    # For picking slip
-    id = database.Column(database.Integer, primary_key=True)
-    part_number = database.Column(database.Text, nullable=False)
-    part_name = database.Column(database.Text, nullable=False)
-    quantity = database.Column(database.Text, nullable=False)
-    # Date picking slip was generated
-    date = database.Column(database.DateTime(timezone=True), default=func.now())
+    date_created = database.Column(database.DateTime(timezone=True))
     
